bb7d4f127d71ff0eee112b9266857148c305a703
Sander Vinter <sander.vinter@gmail.com>
2023-09-28 16:30:23 +0300

    Added navigation and also few changes to the app

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import CurrentWeather from './src/screens/CurrentWeather'
-import UpcomingWeather from './src/screens/UpcomingWeather'
-import City from './src/screens/City'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Counter from './src/demonstration/Counter'
+import Tabs from './src/components/Tabs'
 
+const Tab = createBottomTabNavigator()
 const App = () => {
   return (
-    <View style={styles.container}>
-      <City />
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 export default App
---
+++ src/utilities/weatherType.js
@@ -0,0 +1,42 @@
+export const weatherType = {
+  Thunderstorm: {
+    icon: 'zap',
+    message: ' It could get noisy out there!',
+    backgroundColor: '#000000'
+  },
+  Drizzle: {
+    icon: 'cloud-rain',
+    message: ' It could get a little wet out there!',
+    backgroundColor: '#36454F'
+  },
+  Rain: {
+    icon: 'umbrella',
+    message: 'You will need an umbrella!',
+    backgroundColor: '#0000FF'
+  },
+  Snow: {
+    icon: 'cloud-snow',
+    message: 'You will need a jacket!',
+    backgroundColor: '#7f6065'
+  },
+  Clear: {
+    icon: 'sun',
+    message: 'It is a beautiful day!',
+    backgroundColor: '#E47200'
+  },
+  Clouds: {
+    icon: 'cloud',
+    message: 'It is a little cloudy out there!',
+    backgroundColor: '#363636'
+  },
+  Haze: {
+    icon: 'wind',
+    message: 'It might be a bit damp out there!',
+    backgroundColor: '#58586E'
+  },
+  Mist: {
+    icon: 'align-justify',
+    message: 'It might be hard to see!',
+    backgroundColor: '#3E3E37'
+  }
+}
